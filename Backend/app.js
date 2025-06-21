15e3fbfeddc2170581ef422fdd599c22f67ba767
uzumaki-ak <anikydv38@gmail.com>
2025-06-21 17:16:40 +0530

    Added idea controller and routes, updated backend files

--- Backend/app.js
+++ Backend/app.js
@@ -1,28 +1,30 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from "cors"
-import cookieParser from 'cookie-parser';
-import fileUpload from 'express-fileupload';
-import userRouter from './routes/userRouter.js';
-import swipeRouter from './routes/swipeRouter.js';
-import projectRouter from './routes/projectRouter.js';
-import interactionRouter from './routes/interactionRouter.js';
-import { dbConnection } from './database/dbConnection.js';
-import { errorMiddleware} from "./middlewares/error.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import userRouter from "./routes/userRouter.js";
+import swipeRouter from "./routes/swipeRouter.js";
+import projectRouter from "./routes/projectRouter.js";
+import interactionRouter from "./routes/interactionRouter.js";
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import ideaRoutes from "./routes/ideaRoutes.js";
 
 const app = express();
-dotenv.config({path: "./config/config.env"});
+dotenv.config({ path: "./config/config.env" });
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.FRONTEND_URL,
-    methods:["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}))
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.use(
   fileUpload({
@@ -31,10 +33,11 @@ app.use(
   })
 );
 
-app.use('/api/v1/user',userRouter);
-app.use('/api/v1/swipe',swipeRouter);
-app.use('/api/v1/project',projectRouter);
-app.use('/api/v1/interaction',interactionRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/swipe", swipeRouter);
+app.use("/api/v1/project", projectRouter);
+app.use("/api/v1/interaction", interactionRouter);
+app.use('/api/ideas', ideaRoutes);
 
 dbConnection();
 
